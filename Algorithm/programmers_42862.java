4dfaa5ecdb30972f688b47303c6f041d894539ca
LSH <sohhyang7@gmail.com>
2021-02-19 00:29:37 +0900

    I'm starting to study greedy...

---
+++ Algorithm/programmers_42862.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        return answer;
+    }
+}
+//1. n을 전체 배열로
+//2. 이 중 lost를 제외한 나머지를 일단 answer에
+//3. lost 배열에서 reserve +1 -1 했을 때 값이 포함되어 있는 지 확인
+//4. 중복 주의!!
+//greedy... 어렵
