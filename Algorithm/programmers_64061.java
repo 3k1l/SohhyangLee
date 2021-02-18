f94702636b6ec9d059d5947251f04d591c725b45
LSH <sohhyang7@gmail.com>
2021-02-19 00:14:54 +0900

    use Stack<Integer>

--- Algorithm/programmers_64061.java
+++ Algorithm/programmers_64061.java
@@ -2,20 +2,20 @@ import java.util.*;
 class Solution {
     public int solution(int[][] board, int[] moves) {
         int answer = 0;
-        //Stack<Integer> list = new Stack<>();
-        ArrayList<Integer> list = new ArrayList<>();
-        for(int i=0; i<moves.length; i++){
+        Stack<Integer> stackList = new Stack<>();
+        stackList.push(0);
+
+        for(int move : moves){
             for(int j=0; j<board.length; j++){
-                if(board[j][moves[i]-1] != 0){
-                    System.out.println("board["+j+"]"+"["+(moves[i]-1)+"]"+": " + board[j][moves[i]-1]);
-                    list.add(board[j][moves[i]-1]);
-                   // System.out.println(list.peek());
-                    if(list.get(list.size()-1) == board[j][moves[i]-1]){
-                        System.out.println("same");
-                       // list.pop();
-                       // answer = answer + 1;
+                if(board[j][move-1] != 0){
+                    if(stackList.peek() == board[j][move-1]){
+                    stackList.pop();
+                    answer += 2;
                     }
-                    board[j][moves[i]-1] = 0;
+                    else{
+                        stackList.push(board[j][move-1]);
+                    }
+                    board[j][move-1] = 0;
                     break;
                 }
             }
@@ -23,3 +23,7 @@ class Solution {
         return answer;
     }
 }
+
+//stack.peek -> get top value
+//stack.pop -> delete and print top value
+//stack.push -> add value
