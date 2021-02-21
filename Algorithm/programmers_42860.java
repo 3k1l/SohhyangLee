73f17f6e4a0b0d4917c63a7648b77a2d8ae35a9d
LSH <sohhyang7@gmail.com>
2021-02-22 00:47:13 +0900

    Greedy... Why fail....

--- Algorithm/programmers_42860.java
+++ Algorithm/programmers_42860.java
@@ -1,8 +1,24 @@
+import java.util.*;
 class Solution {
     public int solution(String name) {
         int answer = 0;
+        String[] strName = name.split("");
+
+        for(int i=0; i<strName.length; i++){
+            int text = strName[i].charAt(0) - 65;
+            if(text > 13){
+                answer = answer + (26-text);
+            }
+            else if(text < 13){
+                answer = answer + text;
+            }
+            else if(text == 13){
+                answer = answer + 13;
+            }
+        }
         return answer;
     }
 }
-// 26개...
-//배열에서 26을 기준으로 +-를 해야할 것 같네요
+
+//A~Z : 65~90
+//a~z : 97~122
