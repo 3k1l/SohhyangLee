332f724de2daa93a481a53f4688b612b7ef4a304
LSH <sohhyang7@gmail.com>
2021-02-24 00:47:09 +0900

    Think from back case

--- Algorithm/programmers_42860.java
+++ Algorithm/programmers_42860.java
@@ -6,6 +6,7 @@ class Solution {
 
         for(int i=0; i<strName.length; i++){
             int text = strName[i].charAt(0) - 65;
+            //from front
             if(text > 13){
                 answer = answer + (26-text);
             }
@@ -15,10 +16,15 @@ class Solution {
             else if(text == 13){
                 answer = answer + 13;
             }
+            //from back? A..
+            //A의 갯수를 구해야하나..
         }
+        // 뒤로 가는 테스트 케이스 생각이 필요....
         return answer;
     }
 }
 
 //A~Z : 65~90
 //a~z : 97~122
+// 뒤에서 가는 거가 생각보다 너무 어려움
+//
